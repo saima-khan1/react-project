bd4dac1311b877bff703b63fc823a6ce36e4d598
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2023-09-02 22:19:51 +0200

    removed unwanted packages and added selection of platform

--- game-search-app/src/components/SearchIcon/SearchInput.tsx
+++ game-search-app/src/components/SearchIcon/SearchInput.tsx
@@ -1,28 +1,11 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import './SearchInput.css'
-// import { faSearch } from '@fortawesome/free-solid-svg-icons'
-
-
-// const SearchInput = () => {
-//   return (
-//     <>
-//     <FontAwesomeIcon icon={faSearch}/>
-//     <input className='search-input'  type='text' placeholder='search games...' ></input>
-//     </>
-    
-//   )
-// }
-
-// export default SearchInput
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './SearchInput.css';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import searchIcon from '../../assets/Icons/searchIcon.svg'
 
 const SearchInput = () => {
     return (
         <div className='search-container'>
             <div className='search-box'>
-                <FontAwesomeIcon icon={faSearch} className='search-icon' />
+               <img src={searchIcon} alt='' className='search-icon'/>
                 <input className='search-input' type='text' placeholder='Search games...' />
             </div>
         </div>
